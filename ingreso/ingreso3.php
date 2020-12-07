7a9a01075dc39cb0a21efc33fbdf35cd689d03f9
Giovanni021003 <gordillog100@gmail.com>
2020-12-06 22:07:16 -0600

    Ayudando a ale a subir archivos de ingreso

---
+++ ingreso/ingreso3.php
@@ -0,0 +1,22 @@
+<?php
+$usuario = $_POST['usuario'];
+$password = $_POST['password'];
+$pass = md5($password);
+
+$conexion = mysqli_connect("localhost", "root", "", "nwind");
+$consulta = "SELECT * FROM usuarios WHERE usuario='$usuario' AND password='$pass'";
+$resultado = mysqli_query($conexion, $consulta);
+$filas = mysqli_num_rows($resultado);
+
+if ($filas>0) {
+	header("location:../indexship.php");
+}
+else
+{
+	echo "<script> alert('Ese usuario y/o password no existen');
+			window.location = '../login/Login3.php'; </script>";
+}
+mysqli_free_result($resultado);
+mysqli_close($conexion);
+
+?>
