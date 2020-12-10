c84391253d5f1c0c47fd8350a60d5a47ea674c4c
Giovanni021003 <gordillog100@gmail.com>
2020-12-09 22:09:36 -0600

    Agregando ingreso 5 y 6

---
+++ ingreso/ingreso6.php
@@ -0,0 +1,11 @@
+<?php
+$usuario = $_POST['usuario'];
+$password = $_POST['password'];
+
+$conexion = mysqli_connect("localhost", "root", "", "nwind");
+$consulta = "INSERT INTO usuarios (usuario, password) VALUES ('$usuario',MD5('$password'))";	
+$resultado = mysqli_query($conexion, $consulta);
+header("location:../indexcustom.php");
+mysqli_close($conexion);
+
+?>
